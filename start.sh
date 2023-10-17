2edbb87dbd5d40d503a475f492279f63464e175f
ip <you@example.com>
2023-10-17 22:17:00 +0300

    20240810

--- start.sh
+++ start.sh
@@ -5,7 +5,9 @@ cd $DIR
 
 #uncomment following line to use in python virtual environment
 #source bin/activate
+source /srv/dev-disk-by-uuid-2bb769cc-1798-4c89-b342-b738df3f6d87/appdata/python3-venv/bin/activate
 
-./LYWSD03MMC.py --passive --watchdogtimer 5 #--mqttconfigfile mqtt.conf
-
-
+echo "$(date): start.sh: MI Bluetooth sensor startup..."
+arguments="--passive --battery --rssi --watchdogtimer 60 --callback sendToMQTT.sh"
+python -u ./LYWSD03MMC.py $arguments >> ~/logs/mi_result.log &
+echo "$(date): start.sh: MI Bluetooth sensor startup is started in background"
